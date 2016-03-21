fbe3708ffa3ad6969571e758b1aa7c308d3af8c0
Jacob Mao <jacobmao0416@outlook.com>
2016-03-21 10:25:20 +0800

    finished

--- 100_SameTree/SameTree.swift
+++ 100_SameTree/SameTree.swift
@@ -26,6 +26,18 @@
 
 class Solution {
     func isSameTree(p: TreeNode?, _ q: TreeNode?) -> Bool {
-        
+        guard let pNode = p, let qNode = q else {
+            if p == nil && q == nil {
+                return true
+            } else {
+                return false
+            }
+        }
+
+        let resultOfComparingValue = pNode.val == qNode.val
+        let resultOfComparingLeftNode = self.isSameTree(pNode.left, qNode.left)
+        let resultOfComparingRightNode = self.isSameTree(pNode.right, qNode.right)
+
+        return resultOfComparingValue && resultOfComparingLeftNode && resultOfComparingRightNode
     }
 }
