636b07e78e7e89990c799f9e59e2bd9e1a4d6988
Jacob Mao <jacobmao0416@outlook.com>
2016-03-21 11:17:25 +0800

    finished

--- 242_ValidAnagram/ValidAnagram.swift
+++ 242_ValidAnagram/ValidAnagram.swift
@@ -4,7 +4,24 @@ class Solution {
             return false
         }
 
-        let c1 = [Int](count: 26, repeatedValue: 0)
-        let c2 = [Int](count: 26, repeatedValue: 0)
+        var cList = [Int](count: 26, repeatedValue: 0)
+
+        var currentIndex = s.unicodeScalars.startIndex
+        let endIndex = s.unicodeScalars.endIndex
+        let sUnicodeScalars = s.unicodeScalars
+        let tUnicodeScalars = t.unicodeScalars
+        while currentIndex != endIndex {
+            let value1 = Int(sUnicodeScalars[currentIndex].value) - 97
+            let value2 = Int(tUnicodeScalars[currentIndex].value) - 97
+
+            cList[value1] += 1
+            cList[value2] -= 1
+
+            currentIndex = currentIndex.successor()
+        }
+
+        return !(cList.contains { (value) -> Bool in
+            return value != 0
+        })
     }
 }
