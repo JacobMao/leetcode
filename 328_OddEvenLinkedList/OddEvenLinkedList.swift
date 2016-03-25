830e76e3de3f887604c7c8af4d14f530cf77569a
Jacob Mao <jacobmao0416@outlook.com>
2016-03-25 11:24:22 +0800

    finished

--- 328_OddEvenLinkedList/OddEvenLinkedList.swift
+++ 328_OddEvenLinkedList/OddEvenLinkedList.swift
@@ -21,6 +21,23 @@ public class ListNode {
 
 class Solution {
     func oddEvenList(head: ListNode?) -> ListNode? {
-        
+        if head?.next?.next == nil {
+            return head
+        }
+
+        var lastOddNode = head
+        let firstEvenNode = head?.next
+        var lastEvenNode = head?.next
+
+        while lastEvenNode?.next != nil {
+            lastOddNode?.next = lastEvenNode?.next
+            lastEvenNode?.next = lastOddNode?.next?.next
+            lastOddNode?.next?.next = firstEvenNode
+
+            lastOddNode = lastOddNode?.next
+            lastEvenNode = lastEvenNode?.next
+        }
+
+        return head
     }
 }
