0ad0a462b916f10d025d0208b2195d22d7720751
Jacob Mao <jacobmao0416@outlook.com>
2016-04-05 11:53:37 +0800

    finished

--- 107_BinaryTreeLevelOrderTraversal_II/BinaryTreeLevelOrderTraversal_II.swift
+++ 107_BinaryTreeLevelOrderTraversal_II/BinaryTreeLevelOrderTraversal_II.swift
@@ -25,6 +25,52 @@ public class TreeNode {
 
 class Solution {
     func levelOrderBottom(root: TreeNode?) -> [[Int]] {
+        guard let rootNode = root else {
+            return []
+        }
 
+        var ret: [[Int]] = []
+        var values: [Int] = []
+        var nodes: [TreeNode?] = []
+
+        nodes.append(rootNode)
+        nodes.append(nil)
+
+        while !nodes.isEmpty {
+            guard let currentNode = nodes.removeFirst() else {
+                ret.append(values)
+
+                if !nodes.isEmpty {
+                    values.removeAll()
+                    nodes.append(nil)
+                }
+
+                continue
+            }
+
+            values.append(currentNode.val)
+            if currentNode.left != nil {
+                nodes.append(currentNode.left)
+            }
+            if currentNode.right != nil {
+                nodes.append(currentNode.right)
+            }
+        }
+
+        return ret.reverse()
     }
 }
+
+let a = TreeNode(3)
+let b = TreeNode(9)
+let c = TreeNode(20)
+let d = TreeNode(15)
+let e = TreeNode(7)
+
+a.left = b
+a.right = c
+c.left = d
+c.right = e
+
+let s = Solution()
+print(s.levelOrderBottom(a))
